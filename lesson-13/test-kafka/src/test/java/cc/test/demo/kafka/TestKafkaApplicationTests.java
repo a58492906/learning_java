53d0f81a82bb2ffcfb00aea521fa5c478cad445e
xuejm_dp <623025758@qq.com>
2021-08-01 23:39:10 +0800

    kafka

---
+++ lesson-13/test-kafka/src/test/java/cc/test/demo/kafka/TestKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.test.demo.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
